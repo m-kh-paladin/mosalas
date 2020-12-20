e8aa2ff597098151c789e4dfb4d367d9b779b55f
mohammad <m.paladin.wiz@gmail.com>
2020-12-20 15:26:19 +0330

    finish slider

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@ $(document).ready(function () {
         $(".responsive-nav ul").toggle(400);
     });
 });
+
+$(document).ready(function () {
+    $("#lightSlider").lightSlider({
+        item: 1,
+        rtl: true,
+        enableTouch: true,
+        enableDrag: true,
+        freeMove: true,
+        swipeThreshold: 40,
+        speed: 1300, 
+        auto: true,
+        loop: true,
+        slideEndAnimation: true,
+        pause: 3000,
+
+    });
+});
+
+
